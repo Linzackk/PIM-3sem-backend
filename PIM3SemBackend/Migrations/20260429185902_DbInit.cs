5a8d9365e06aa5c4d6f124bb3a0fcabc9143937b
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-29 16:00:51 -0300

    feat Migrations realizadas para o Banco SQL

---
+++ PIM3SemBackend/Migrations/20260429185902_DbInit.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PIM_3sem_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Perfis",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perfis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdFuncionario = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    DataInicio = table.Column<DateOnly>(type: "date", nullable: false),
+                    DataFim = table.Column<DateOnly>(type: "date", nullable: true),
+                    MotivoAlteracao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Salario = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Cargo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IdDepartamento = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdGerente = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Funcionarios_Departamentos_IdDepartamento",
+                        column: x => x.IdDepartamento,
+                        principalTable: "Departamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Funcionarios_Funcionarios_IdGerente",
+                        column: x => x.IdGerente,
+                        principalTable: "Funcionarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdPerfil = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Perfis_IdPerfil",
+                        column: x => x.IdPerfil,
+                        principalTable: "Perfis",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PontosRegistro",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdFuncionario = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateOnly>(type: "date", nullable: false),
+                    HoraEntrada = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    HoraSaida = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PontosRegistro", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PontosRegistro_Funcionarios_IdFuncionario",
+                        column: x => x.IdFuncionario,
+                        principalTable: "Funcionarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_IdDepartamento",
+                table: "Funcionarios",
+                column: "IdDepartamento");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_IdGerente",
+                table: "Funcionarios",
+                column: "IdGerente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PontosRegistro_IdFuncionario",
+                table: "PontosRegistro",
+                column: "IdFuncionario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_IdPerfil",
+                table: "Usuarios",
+                column: "IdPerfil");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PontosRegistro");
+
+            migrationBuilder.DropTable(
+                name: "Salarios");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+
+            migrationBuilder.DropTable(
+                name: "Perfis");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+        }
+    }
+}
