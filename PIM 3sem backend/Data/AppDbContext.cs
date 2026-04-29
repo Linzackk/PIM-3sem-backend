fde579cbcafeb5debccd0013c1f135ee0e083499
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-29 15:16:39 -0300

    feat Criação das Tabelas para o Banco de Dados

---
+++ PIM 3sem backend/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIM_3sem_backend.Models;
+
+namespace PIM_3sem_backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Perfil> Perfis { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Salario> Salarios { get; set; }
+        public DbSet<PontoRegistro> PontosRegistro { get; set; }
+
+    }
+}
