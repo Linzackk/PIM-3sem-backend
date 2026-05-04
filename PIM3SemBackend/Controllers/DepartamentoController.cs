637d427bc2a875ebc59bea38535f7229da841780
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 15:08:54 -0300

    feat Controller de Departamentos

---
+++ PIM3SemBackend/Controllers/DepartamentoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PIM_3sem_backend.Services.Departamentos;
+
+namespace PIM_3sem_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartamentoController : ControllerBase
+    {
+        private readonly IDepartamentoService _service;
+        public DepartamentoController(IDepartamentoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{departamentoId}")]
+        public async Task<IActionResult> ObterPorId(Guid departamentoId)
+        {
+            var departamento = await _service.ObterPorId(departamentoId);
+            return Ok(departamento);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodos()
+        {
+            var departamentos = await _service.ObterTodos();
+            return Ok(departamentos);
+        }
+    }
+}
