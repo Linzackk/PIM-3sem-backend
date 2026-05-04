d51f977b00e45abb3c3410fc3a381b6472e57d79
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 16:56:33 -0300

    feat Criação de DTOs para Funcionario

---
+++ PIM3SemBackend/DTOs/Funcionarios/FuncionarioResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PIM_3sem_backend.DTOs.Funcionarios
+{
+    public class FuncionarioResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+        public string Cargo { get; set; }
+        public string Departamento { get; set; }
+        public string? Gerente { get; set; } = string.Empty;
+    }
+}
---
+++ PIM3SemBackend/DTOs/Funcionarios/FuncionarioUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PIM_3sem_backend.DTOs.Funcionarios
+{
+    public class FuncionarioUpdateDTO
+    {
+        public decimal? Salario { get; set; }
+        public string? Cargo { get; set; }
+    }
+}
