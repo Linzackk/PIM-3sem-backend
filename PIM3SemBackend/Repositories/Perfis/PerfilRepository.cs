62e25d3a4f6ed61e5143f026df3c713e3d3b1d2f
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 14:22:55 -0300

    feat Repository de Perfil

---
+++ PIM3SemBackend/Repositories/Perfis/PerfilRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIM_3sem_backend.Data;
+using PIM_3sem_backend.Models;
+
+namespace PIM_3sem_backend.Repositories.Perfis
+{
+    public class PerfilRepository : IPerfilRepository
+    {
+        private readonly AppDbContext _context;
+        public PerfilRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Perfil?> ObterPorId(Guid perfilId)
+        {
+            return _context.Perfis.FirstOrDefaultAsync(perfil => perfil.Id.Equals(perfilId));
+        }
+
+        public Task<List<Perfil>> ObterTodos()
+        {
+            return _context.Perfis.ToListAsync();
+        }
+    }
+}
