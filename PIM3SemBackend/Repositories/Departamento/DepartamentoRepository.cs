b2f0a97dd46fa8880ff40553a40a96727854ea82
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 15:01:45 -0300

    feat Repository de Departamentos

---
+++ PIM3SemBackend/Repositories/Departamento/DepartamentoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIM_3sem_backend.Data;
+using PIM_3sem_backend.Models;
+
+namespace PIM_3sem_backend.Repositories.Departamentos
+{
+    public class DepartamentoRepository : IDepartamentoRepository
+    {
+        private readonly AppDbContext _context;
+        public DepartamentoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Departamento?> ObterPorId(Guid departamentoId)
+        {
+            return _context.Departamentos.FirstOrDefaultAsync(departamento => departamento.Id.Equals(departamentoId));
+        }
+
+        public Task<List<Departamento>> ObterTodos()
+        {
+            return _context.Departamentos.ToListAsync();
+        }
+    }
+}
---
+++ PIM3SemBackend/Repositories/Departamento/IDepartamentoRepository.cs
@@ -0,0 +1,10 @@
+﻿using PIM_3sem_backend.Models;
+
+namespace PIM_3sem_backend.Repositories.Departamentos
+{
+    public interface IDepartamentoRepository
+    {
+        Task<Departamento?> ObterPorId(Guid departamentoId);
+        Task<List<Departamento>> ObterTodos();
+    }
+}
