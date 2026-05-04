d27c0b9cdd9a482af4cf58ebb562816c66575bf5
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 16:51:53 -0300

    feat Repository de Funcionario

---
+++ PIM3SemBackend/Repositories/Funcionarios/FuncionarioRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIM_3sem_backend.Data;
+using PIM_3sem_backend.Models;
+
+namespace PIM_3sem_backend.Repositories.Funcionarios
+{
+    public class FuncionarioRepository : IFuncionarioRepository
+    {
+        private readonly AppDbContext _context;
+        public FuncionarioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CriarFuncionario(Funcionario novoFuncionario)
+        {
+            await _context.Funcionarios.AddAsync(novoFuncionario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Funcionario?> ObterPorId(Guid funcionarioId)
+        {
+            return await _context.Funcionarios
+                .Include(f => f.Departamento)
+                .Include(f => f.Gerente)
+                .FirstOrDefaultAsync(f => f.Id.Equals(funcionarioId));
+        }
+
+        public async Task<List<Funcionario>> ObterTodos()
+        {
+            return await _context.Funcionarios.ToListAsync();
+        }
+
+        public async Task<List<Funcionario>> ObterFuncionarioDoGerente(Guid gerenteId)
+        {
+            var funcionarios = await _context.Funcionarios
+                .Include(f => f.Departamento)
+                .Include(f => f.Gerente)
+                .Where(f => f.IdGerente.Equals(gerenteId))
+                .AsNoTracking()
+                .ToListAsync();
+
+            return funcionarios;
+        }
+
+        public async Task AtualizarFuncionario(Funcionario funcionarioAtualizado)
+        {
+            _context.Funcionarios.Update(funcionarioAtualizado);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoverFuncionario(Funcionario funcionario)
+        {
+            _context.Funcionarios.Remove(funcionario);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
