d52ca9b6c4cb1de9de28026cc5dd093e849a5295
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 14:38:32 -0300

    feat Criação de Service para Perfil

---
+++ PIM3SemBackend/Services/Perfis/IPerfilService.cs
@@ -0,0 +1,10 @@
+﻿using PIM_3sem_backend.DTOs.Perfis;
+
+namespace PIM_3sem_backend.Services.Perfis
+{
+    public interface IPerfilService
+    {
+        Task<PerfilResponseDTO> ObterPorId(Guid perfilId);
+        Task<IReadOnlyCollection<PerfilResponseDTO>> ObterTodos();
+    }
+}
