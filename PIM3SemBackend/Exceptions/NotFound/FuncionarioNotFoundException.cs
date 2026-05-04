5fbf6073ccbd48a52846f218e1bdc65d5546b02e
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 17:15:25 -0300

    feat Exception para Funcionario NotFound

---
+++ PIM3SemBackend/Exceptions/NotFound/FuncionarioNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace PIM_3sem_backend.Exceptions.NotFound
+{
+    public class FuncionarioNotFoundException : NotFoundException
+    {
+        public FuncionarioNotFoundException()
+            : base("Funcionario não encontrado.") { }
+    }
+}
