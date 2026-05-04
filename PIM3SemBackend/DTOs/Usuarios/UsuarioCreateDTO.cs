bf1121414efb13758914a10e1f672aecb55bb51e
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-05-04 15:36:32 -0300

    feat DTOs para Criação e Resposta de Usuarios

---
+++ PIM3SemBackend/DTOs/Usuarios/UsuarioCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PIM_3sem_backend.DTOs.Usuarios
+{
+    public class UsuarioCreateDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Guid IdPerfil { get; set; }
+    }
+}
---
+++ PIM3SemBackend/DTOs/Usuarios/UsuarioResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PIM_3sem_backend.DTOs.Usuarios
+{
+    public class UsuarioResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Perfil { get; set; }
+        public string Status { get; set; }
+    }
+}
